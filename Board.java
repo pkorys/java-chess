76404cf6a22f1b6f495bf0103804279a948ee820
pkorys <85293282+pkorys@users.noreply.github.com>
2021-06-03 18:34:06 +0200

    Create Board.java
    Methods to prepare board to play, set pieces on fields, print board and manage pieces.

---
+++ Board.java
@@ -0,0 +1,184 @@
+import java.util.ArrayList;
+
+public class Board {
+    public Field[] fields = new Field[64];
+
+    public void setFields(){
+        for(int i = 0; i < fields.length; i++){
+            fields[i] = new Field((i%8)+1, 8-i/8);
+        }
+        setFigures();
+    }
+
+    private void setFigures(){
+        for(int i = 0; i < fields.length; i++){
+            if(i==0 || i == 7)
+                fields[i].setFigureAtField(new Rook("Black"));
+            else if (i == 56 || i == 63)
+                fields[i].setFigureAtField(new Rook("White"));
+            else if (i == 1 || i == 6)
+                fields[i].setFigureAtField(new Knight("Black"));
+            else if(i == 57 || i == 62)
+                fields[i].setFigureAtField(new Knight("White"));
+            else if(i == 2 || i == 5)
+                fields[i].setFigureAtField(new Bishop("Black"));
+            else if(i == 58 || i == 61)
+                fields[i].setFigureAtField(new Bishop("White"));
+            else if(i == 3)
+                fields[i].setFigureAtField(new Queen("Black"));
+            else if(i == 59)
+                fields[i].setFigureAtField(new Queen("White"));
+            else if(i == 4)
+                fields[i].setFigureAtField(new King("Black"));
+            else if(i == 60)
+                fields[i].setFigureAtField(new King("White"));
+            else if(i > 7 && i < 16)
+                fields[i].setFigureAtField(new Pawn("Black"));
+            else if(i > 47 && i < 56)
+                fields[i].setFigureAtField(new Pawn("White"));
+            else
+                fields[i].setFigureAtField(null);
+        }
+    }
+
+    public void printBoard(){
+        System.out.println("   a b c d e f g h");
+        for(int i = 0; i < 64; i++){
+            if(i%8==0){
+                System.out.println();
+                System.out.print((8-i/8) + "  ");
+            }
+
+            fields[i].printFigure();
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    public Field getField(String pos){
+        int x;
+        int y;
+
+        if(pos.length() > 1)
+            y = Character.getNumericValue(pos.charAt(1));
+        else
+            y = -1;
+
+        if(y < 1 || y > 8)
+            y = -1;
+
+        switch (pos.charAt(0)){
+            case 'a':
+                x = 1;
+                break;
+            case 'b':
+                x = 2;
+                break;
+            case 'c':
+                x = 3;
+                break;
+            case 'd':
+                x = 4;
+                break;
+            case 'e':
+                x = 5;
+                break;
+            case 'f':
+                x = 6;
+                break;
+            case 'g':
+                x = 7;
+                break;
+            case 'h':
+                x = 8;
+                break;
+            default:
+                x = -1;
+                break;
+        }
+
+        return findField(x, y);
+    }
+    public Field findField(int x, int y){
+        if(x == -1 || y == -1)
+            return null;
+        else
+            return fields[8*(8-y)+x-1];
+    }
+
+    public boolean isMyFigure(String move, String color){
+        if(getField(move) == null || getField(move).getFigureAtField() == null){
+            return false;
+        }
+
+        else if(getField(move).getFigureAtField().getColor() != color){
+            return false;
+        }
+
+        else
+            return true;
+    }
+
+    public boolean canIMoveTo(String move, String color){
+        if(getField(move) == null){ //Field out of board
+            return false;
+        }
+
+
+        else if(getField(move).getFigureAtField() != null && getField(move).getFigureAtField().getColor() == color)
+            return false;
+
+        else
+            return true;
+    }
+
+    public void addFields(ArrayList<Field> fieldsToTravel) {
+        int startX = fieldsToTravel.get(0).getPos()[0];
+        int destinyX = fieldsToTravel.get(fieldsToTravel.size()-1).getPos()[0];
+        int startY = fieldsToTravel.get(0).getPos()[1];
+        int destinyY = fieldsToTravel.get(fieldsToTravel.size()-1).getPos()[1];
+        int it = 1;
+
+        if(startX > destinyX){
+            for(int i = startX - it; i > destinyX; i--){
+                if(startY < destinyY){
+                    for(int j = startY+it; j < destinyY; j++)
+                        fieldsToTravel.add(fieldsToTravel.size()-1, findField(i,j));
+                }
+                else{
+                    for(int j = destinyY+it; j < startY; j++)
+                        fieldsToTravel.add(fieldsToTravel.size()-1, findField(i,j));
+                }
+                it++;
+            }
+        }
+        else if(startX == destinyX){
+            if(startY < destinyY){
+                for(int j = startY; j < destinyY; j++)
+                    fieldsToTravel.add(fieldsToTravel.size()-1, findField(startX,j));
+            }
+            else{
+                for(int j = startY; j > destinyY; j--)
+                    fieldsToTravel.add(fieldsToTravel.size()-1, findField(startX,j));
+            }
+        }
+        else{
+            for(int i = startX+it; i < destinyX; i++){
+                if(startY > destinyY){
+                    for(int j = destinyY+it; j < startY; j++)
+                        fieldsToTravel.add(fieldsToTravel.size()-1, findField(i,j));
+                }
+                else{
+
+                    for(int j = startY+it; j < destinyY; j++)
+                        fieldsToTravel.add(fieldsToTravel.size()-1, findField(i,j));
+                }
+                it++;
+            }
+        }
+
+        for(int i = 1; i < fieldsToTravel.size(); i++){
+            if(fieldsToTravel.get(i) == fieldsToTravel.get(0))
+                fieldsToTravel.remove(i);
+        }
+    }
+}
