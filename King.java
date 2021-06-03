f1642466cba1d7c83aaa2f2deb1a8bdc01fc039a
pkorys <85293282+pkorys@users.noreply.github.com>
2021-06-03 18:58:23 +0200

    Class and methods describing king behavior

---
+++ King.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class King extends Figure{
+    boolean haveMoved = false; //Did King moved before?
+
+    public King(String color){
+        super("King", color);
+    }
+
+    @Override
+    public boolean canMove(ArrayList<Field> fields) {
+        int verticalMove = fields.get(0).getPos()[1] - fields.get(fields.size()-1).getPos()[1]; //Distance King want to travel vertical
+        int horizontalMove = fields.get(0).getPos()[0] - fields.get(fields.size()-1).getPos()[0]; //Distance King want to travel horizontal
+
+        if((Math.abs(horizontalMove) == 1 && Math.abs(verticalMove) == 0) || (Math.abs(horizontalMove) == 0 && Math.abs(verticalMove) == 1)){
+            haveMoved = true;
+            return true;
+        }
+
+        else return false;
+    }
+
+
+}
