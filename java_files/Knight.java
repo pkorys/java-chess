fd290a825453ab08488b49c50fa409a39fd8b2db
pkorys <85293282+pkorys@users.noreply.github.com>
2021-06-11 17:19:47 +0200

    Added check mechanic to Knight class

--- java_files/Knight.java
+++ java_files/Knight.java
@@ -19,4 +19,39 @@ public class Knight extends Piece {
     private boolean isItProperKnightHorizontalMove(){
        return (Math.abs(moveInHorizontal()) == 2 && Math.abs(moveInVertical()) == 1);
     }
+
+    @Override
+    protected void checkFields() {
+        int[] ones = new int[]{-1, 1};
+        int[] twos = new int[]{-2, 2};
+
+        for(int i = 0; i < ones.length; i++){
+            for(int j=0; j< twos.length; j++)
+                checkIfFieldExists(myPosition.getHorizontalPosition()+ones[i], myPosition.getVerticalPosition()+twos[j]);
+        }
+
+        for(int i = 0; i < ones.length; i++){
+            for(int j=0; j< twos.length; j++){
+                checkIfFieldExists(myPosition.getHorizontalPosition()+twos[i], myPosition.getVerticalPosition()+ones[j]);
+            }
+        }
+
+    }
+
+    @Override
+    protected void uncheckFields() {
+        int[] ones = new int[]{-1, 1};
+        int[] twos = new int[]{-2, 2};
+
+        for(int i = 0; i < ones.length; i++){
+            for(int j=0; j< twos.length; j++)
+                uncheckIfFieldExists(myPosition.getHorizontalPosition()+ones[i], myPosition.getVerticalPosition()+twos[j]);
+        }
+
+        for(int i = 0; i < ones.length; i++){
+            for(int j=0; j< twos.length; j++){
+                uncheckIfFieldExists(myPosition.getHorizontalPosition()+twos[i], myPosition.getVerticalPosition()+ones[j]);
+            }
+        }
+    }
 }
